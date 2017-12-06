cf8ad45d88b464bd0cd8eda041c69ece5c6d67f7
Uzi <lzijbuan@gmail.com>
2017-12-06 10:41:27 +0800

    SQLGenerator now accepts ArrayList

--- java-code/Detection_Algorithm/src/SQL_Generator/SQLGenerator.java
+++ java-code/Detection_Algorithm/src/SQL_Generator/SQLGenerator.java
@@ -1,4 +1,6 @@
 
+import java.util.*;
+
 public class SQLGenerator {
     static class Entity {
         public String name;
@@ -22,15 +24,28 @@ public class SQLGenerator {
         }
         return sql.toString();
     }
+    public String generateFrom(ArrayList<String> entities) {
+        String alphabet = "abcdefghijklmnopqrstuvwxyz";
+        StringBuilder sql = new StringBuilder();
+        sql.append("Select 1 from entity_Table where ");
+        for (int i = 0; i < entities.size(); i++)
+            sql.append("variable_" + alphabet.charAt(i) + " = " + entities.get(i) + " and ");
+        return sql.toString();
+    }
     public static void main(String[] args) {
-        Entity[] entities_test1 = new Entity[2];
-        entities[0] = new Entity("car_age_days", "more");
-        entities[1] = new Entity("7", "");
-        System.out.println(new SQLGenerator().generateFrom(entities_test1));
+        Entity[] entitiesTest1 = new Entity[2];
+        entitiesTest1[0] = new Entity("car_age_days", "more");
+        entitiesTest1[1] = new Entity("7", "");
+        System.out.println(new SQLGenerator().generateFrom(entitiesTest1));
 
-        Entity[] entities_test2 = new Entity[2];
-        entities[0] = new Entity("car_age_days", "more");
-        entities[1] = new Entity("7", "");
-        System.out.println(new SQLGenerator().generateFrom(entities_test1));        
+        Entity[] entitiesTest2 = new Entity[2];
+        entitiesTest2[0] = new Entity("car_age_days", "more");
+        entitiesTest2[1] = new Entity("7", "");
+        System.out.println(new SQLGenerator().generateFrom(entitiesTest2));
+
+        ArrayList<String> arrayList = new ArrayList<String>();
+        arrayList.add("vehicleage");
+        arrayList.add("7");
+        System.out.println(new SQLGenerator().generateFrom(arrayList));
     }
 }
